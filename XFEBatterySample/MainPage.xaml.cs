95394e6e47d38c9ecf8cf00a47cd69fd79d7f9c9
Gerald Versluis <gerald@verslu.is>
2020-12-21 21:13:08 +0100

    Implement Battery

--- XFEBatterySample/MainPage.xaml.cs
+++ XFEBatterySample/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Essentials;
 
 namespace XFEBatterySample
 {
@@ -13,6 +14,17 @@ namespace XFEBatterySample
         public MainPage()
         {
             InitializeComponent();
+
+            BatteryLevel.Text = (Battery.ChargeLevel * 100).ToString() + "%";
+
+            EnergySaver.Text = Battery.EnergySaverStatus.ToString();
+
+            Battery.BatteryInfoChanged += Battery_BatteryInfoChanged;
+        }
+
+        private void Battery_BatteryInfoChanged(object sender, BatteryInfoChangedEventArgs e)
+        {
+            BatteryLevel.Text = (e.ChargeLevel * 100).ToString() + "%";
         }
     }
 }
